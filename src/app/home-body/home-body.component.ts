603c43537759611af4a942a9a74f2f0649998147
arjun <arjun.raju@nestgroup.net>
2024-01-23 15:34:50 +0530

    template icon added

--- src/app/home-body/home-body.component.ts
+++ src/app/home-body/home-body.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-home-body',
   templateUrl: './home-body.component.html',
-  styleUrls: ['./home-body.component.css']
+  styleUrls: ['./home-body.component.css'],
+  // encapsulation: ViewEncapsulation.None
 })
 export class HomeBodyComponent implements OnInit {
 
