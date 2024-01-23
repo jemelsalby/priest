0fb18188c2b16f587a22ec8cd238d2faa3f3a8f2
arjun <arjun.raju@nestgroup.net>
2024-01-23 10:39:56 +0530

    template for redesign

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SignInComponent } from './sign-in/sign-in.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
-  { path: 'sign-in', component: SignInComponent },
-  // { path: 'sign-up', component:SignInComponent, data: { isSignIn: false} },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'profile', component: ProfileComponent },
-  { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 import { AuthService } from './auth.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SignInComponent } from './sign-in/sign-in.component';
-import { FormsModule } from '@angular/forms';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SignInComponent,
-    DashboardComponent,
-    ProfileComponent,
-    
+    AppComponent,    
   ],
   imports: [
     BrowserModule,
