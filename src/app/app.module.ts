384fee21dee85708fc478889ed82ffac57dc3ef0
jemelsalby <jemelsalby3@gmail.com>
2024-02-01 11:27:33 +0530

    added aboutus, login , notifi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { AboutusComponent } from './aboutus/aboutus.component';
 import { ViewInstituteComponent } from './institutions/view-institute/view-institute.component';
 import { InstituteListComponent } from './institutions/institute-list/institute-list.component';
 import { BackButtonComponent } from './shared/back-button/back-button.component';
-import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { PagenotfoundComponent } from './shared/pagenotfound/pagenotfound.component';
 import { NotificationsComponent } from './notifications/notifications.component';
 import { LoginComponent } from './login/login.component';
 
