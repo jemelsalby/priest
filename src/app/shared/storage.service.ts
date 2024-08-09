92d71bf25959a1c8bc08682e51bf631c01f048ed
Arjun <arjunraju122001@gmail.com>
2024-08-10 00:45:06 +0530

    image upload name append

--- src/app/shared/storage.service.ts
+++ src/app/shared/storage.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,8 @@ export class StorageService {
 
   async imageUpload(file: any, partialPath: string): Promise<string> {
     if (file) {
-      const path = `images/${partialPath}/${file.name}`;
+      let r = (Math.random() + 1).toString(36).substring(7);
+      const path = `images/${partialPath}/${file.name}-${r}`;
       const uploadTask = await this.fireStorage.upload(path, file);
       const url = await uploadTask.ref.getDownloadURL();
       console.log(url);
